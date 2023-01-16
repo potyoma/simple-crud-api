390e04548129708fed57fb49f2eecbdf17a4cf3c
potyoma <artyomnovosyolov254@gmail.com>
2023-01-16 18:31:28 +0400

    feat: error messages

---
+++ src/constants.ts
@@ -0,0 +1,7 @@
+export const NOT_FOUND = "Sorry, such entity does not exist."
+
+export const INVALID_UUID = "The provided id is not UUID"
+
+export const INVALID_MODEL = "The provided model is invalid"
+
+export const SERVER_ERROR = "Something went wrong"
